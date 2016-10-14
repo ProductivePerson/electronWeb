574d264db226258d34a566f9c6d2da68a8d7770c
Tim Clark <TC.Clark25@gmail.com>
2016-10-14 18:08:18 -0400

    refactor: switched app to React component

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const axios = require('axios');
 
 app = express();
 
 // app.use('/modules', express.static(__dirname + '/node_modules/'));
 app.use('/', express.static(__dirname));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/exRates', function (req, res) {
+  console.log("ding ding ding");
+  axios.get('https://blockchain.info/ticker')
+      .then(response => {
+        console.log(response.data);
+        res.json(response.data);
+      })
+      .catch(err => console.error("ERROR", err));
+});
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
